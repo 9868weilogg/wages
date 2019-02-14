ec71494e7f1b3bee475014637d2ef3e5138b4e3e
Cheah Wei Logg <william.wlcheah@gmail.com>
2019-02-14 22:28:38 +0800

    do not use count() to count string, use instead substr([0],-9,-5)

--- app/Models/EndOfDayData.php
+++ app/Models/EndOfDayData.php
@@ -160,7 +160,7 @@ class EndOfDayData extends Model
                     'low' => $data[6],
                     'close' => $data[7],
                     'volume' => $data[8],
-                    'code' => substr($data[0],count($data[0])-9,count($data[0])-5),
+                    'code' => substr($data[0],-9,-5),
                     'created_at' => Carbon::createFromFormat('Ymd', $dateTxt)->timestamp,
                   ]);
 
