53f12a2563a9a8b7aee597bb6e3d322c47a44c45
Cheah Wei Logg <william.wlcheah@gmail.com>
2019-02-14 21:35:26 +0800

    Fix cron not working, due to .env connect to tenant connection instead of default mysql

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('command:updateeod')
-                  ->everyMinute();
+                  ->everyFiveMinutes();
                   // ->dailyAt('01:00');
 
     }
